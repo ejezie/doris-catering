5e1bee56004fbf6f4af5fef9eaced92b334ebc6a
ejezie <shady07chinex@gmail.com>
2021-06-12 17:13:33 +0100

    landing page start

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,27 +1,242 @@
 import React from "react";
-import pink from "../images/food.jpg";
+import logo from "../images/logo.png";
 
 function LandingPage() {
   return (
     <div className="landing__page">
-         <img src={pink} alt="background" />
-      <div className="header">
-        <div className="headertext__wrap">
-          <h1 className="header__text">
+      <section className="header__wrap">
+        <img className="logo" src={logo} alt="logo" />
+        <div className="head__span__wrap">
+          <h1 className="header__text1">
             <span>D</span>
+          </h1>
+          <h1 className="header__text2">
             <span>O</span>
+          </h1>
+          <h1 className="header__text3">
             <span>R</span>
+          </h1>
+          <h1 className="header__text4">
             <span>A</span>
+          </h1>
+          <h1 className="header__text5">
             <span>K</span>
+          </h1>
+          <h1 className="header__text6">
             <span>I</span>
+          </h1>
+          <h1 className="header__text7">
             <span>T</span>
+          </h1>
+          <h1 className="header__text8">
             <span>C</span>
+          </h1>
+          <h1 className="header__text9">
             <span>H</span>
+          </h1>
+          <h1 className="header__text10">
             <span>E</span>
+          </h1>
+          <h1 className="header__text11">
             <span>N</span>
           </h1>
         </div>
-      </div>
+
+        <div className="header__span__wrap"></div>
+        <div className="line"></div>
+      </section>
+      {/*  */}
+      {/*  */}
+      <section className="text__section">
+        <div className="text__wrap__one">
+          <h3 className="text__head">
+            <span className="text">
+              I make the best cake you can taste anywhere
+            </span>
+            <span className="mask"></span>
+          </h3>
+          <h3 className="text__head">
+            <span className="text">
+              I make the best cake you can taste anywhere
+            </span>
+            <span className="mask"></span>
+          </h3>
+          <h3 className="text__head">
+            <span className="text">
+              I make the best cake you can taste anywhere
+            </span>
+            <span className="max"></span>
+          </h3>
+          <h3 className="text__head">
+            <span className="text">
+              I make the best cake you can taste anywhere
+            </span>
+            <span className="mask"></span>
+          </h3>
+          <h3 className="text__head">
+            <span className="text">
+              I make the best cake you can taste anywhere
+            </span>
+            <span className="mask"></span>
+          </h3>
+          <h3 className="text__head">
+            <span className="text">
+              I make the best cake you can taste anywhere
+            </span>
+            <span className="mask"></span>
+          </h3>
+          <h3 className="text__head">
+            <span className="text">
+              I make the best cake you can taste anywhere
+            </span>
+            <span className="mask"></span>
+          </h3>
+          <h3 className="text__head">
+            <span className="text">
+              I make the best cake you can taste anywhere
+            </span>
+            <span className="mask"></span>
+          </h3>
+          <h3 className="text__head">
+            <span className="text">
+              I make the best cake you can taste anywhere
+            </span>
+            <span className="mask"></span>
+          </h3>
+          <h3 className="text__head">
+            <span className="text">
+              I make the best cake you can taste anywhere
+            </span>
+            <span className="mask"></span>
+          </h3>
+        </div>
+        {/*  */}
+        <div className="text__wrap__two">
+          <h3 className="text__head">
+            <span className="text">
+              I make the best cake you can taste anywhere
+            </span>
+            <span className="mask"></span>
+          </h3>
+          <h3 className="text__head">
+            <span className="text">
+              I make the best cake you can taste anywhere
+            </span>
+            <span className="mask"></span>
+          </h3>
+          <h3 className="text__head">
+            <span className="text">
+              I make the best cake you can taste anywhere
+            </span>
+            <span className="mask"></span>
+          </h3>
+          <h3 className="text__head">
+            <span className="text">
+              I make the best cake you can taste anywhere
+            </span>
+            <span className="mask"></span>
+          </h3>
+          <h3 className="text__head">
+            <span className="text">
+              I make the best cake you can taste anywhere
+            </span>
+            <span className="mask"></span>
+          </h3>
+          <h3 className="text__head">
+            <span className="text">
+              I make the best cake you can taste anywhere
+            </span>
+            <span className="mask"></span>
+          </h3>
+          <h3 className="text__head">
+            <span className="text">
+              I make the best cake you can taste anywhere
+            </span>
+            <span className="mask"></span>
+          </h3>
+          <h3 className="text__head">
+            <span className="text">
+              I make the best cake you can taste anywhere
+            </span>
+            <span className="mask"></span>
+          </h3>
+          <h3 className="text__head">
+            <span className="text">
+              I make the best cake you can taste anywhere
+            </span>
+            <span className="mask"></span>
+          </h3>
+          <h3 className="text__head">
+            <span className="text">
+              I make the best cake you can taste anywhere
+            </span>
+            <span className="mask"></span>
+          </h3>
+        </div>
+        {/*  */}
+        <div className="text__wrap__three">
+          <h3 className="text__head">
+            <span className="text">
+              I make the best cake you can taste anywhere
+            </span>
+            <span className="mask"></span>
+          </h3>
+          <h3 className="text__head">
+            <span className="text">
+              I make the best cake you can taste anywhere
+            </span>
+            <span className="mask"></span>
+          </h3>
+          <h3 className="text__head">
+            <span className="text">
+              I make the best cake you can taste anywhere
+            </span>
+            <span className="mask"></span>
+          </h3>
+          <h3 className="text__head">
+            <span className="text">
+              I make the best cake you can taste anywhere
+            </span>
+            <span className="mask"></span>
+          </h3>
+          <h3 className="text__head">
+            <span className="text">
+              I make the best cake you can taste anywhere
+            </span>
+            <span className="mask"></span>
+          </h3>
+          <h3 className="text__head">
+            <span className="text">
+              I make the best cake you can taste anywhere
+            </span>
+            <span className="mask"></span>
+          </h3>
+          <h3 className="text__head">
+            <span className="text">
+              I make the best cake you can taste anywhere
+            </span>
+            <span className="mask"></span>
+          </h3>
+          <h3 className="text__head">
+            <span className="text">
+              I make the best cake you can taste anywhere
+            </span>
+            <span className="mask"></span>
+          </h3>
+          <h3 className="text__head">
+            <span className="text">
+              I make the best cake you can taste anywhere
+            </span>
+            <span className="mask"></span>
+          </h3>
+          <h3 className="text__head">
+            <span className="text">
+              I make the best cake you can taste anywhere
+            </span>
+            <span className="mask"></span>
+          </h3>
+        </div>
+      </section>
     </div>
   );
 }
