057e28b67d9c316b92eeeefaeb01365013158b9b
ejezie <shady07chinex@gmail.com>
2021-06-15 02:13:21 +0100

    trying parallax

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -35,51 +35,51 @@ function LandingPage() {
 
   return (
     <div className="landing__page">
-        <Parallax className="header__wrap" speed={-20}>
-          <img className="logo" src={logo} alt="logo" />
-          <div className="head__span__wrap">
-            <h1 className="header__text1">
-              <span>D</span>
-            </h1>
-            <h1 className="header__text2">
-              <span>O</span>
-            </h1>
-            <h1 className="header__text3">
-              <span>R</span>
-            </h1>
-            <h1 className="header__text4">
-              <span>A</span>
-            </h1>
-            <h1 className="header__text5">
-              <span>K</span>
-            </h1>
-            <h1 className="header__text6">
-              <span>I</span>
-            </h1>
-            <h1 className="header__text7">
-              <span>T</span>
-            </h1>
-            <h1 className="header__text8">
-              <span>C</span>
-            </h1>
-            <h1 className="header__text9">
-              <span>H</span>
-            </h1>
-            <h1 className="header__text10">
-              <span>E</span>
-            </h1>
-            <h1 className="header__text11">
-              <span>N</span>
-            </h1>
-          </div>
+      <Parallax className="header__wrap" speed={600}>
+        <img className="logo" src={logo} alt="logo" />
+        <div className="head__span__wrap">
+          <h1 className="header__text1">
+            <span>D</span>
+          </h1>
+          <h1 className="header__text2">
+            <span>O</span>
+          </h1>
+          <h1 className="header__text3">
+            <span>R</span>
+          </h1>
+          <h1 className="header__text4">
+            <span>A</span>
+          </h1>
+          <h1 className="header__text5">
+            <span>K</span>
+          </h1>
+          <h1 className="header__text6">
+            <span>I</span>
+          </h1>
+          <h1 className="header__text7">
+            <span>T</span>
+          </h1>
+          <h1 className="header__text8">
+            <span>C</span>
+          </h1>
+          <h1 className="header__text9">
+            <span>H</span>
+          </h1>
+          <h1 className="header__text10">
+            <span>E</span>
+          </h1>
+          <h1 className="header__text11">
+            <span>N</span>
+          </h1>
+        </div>
 
-          <div className="header__span__wrap"></div>
-          <div className="line"></div>
-        </Parallax>
+        <div className="header__span__wrap"></div>
+        <div className="line"></div>
+      </Parallax>
       {/*  */}
       {/*  */}
       <section className="text__section">
-        <div className="text__wrap__one anim-text">
+        <Parallax className="text__wrap__one anim-text" speed={-40}>
           <h3 className="text__head">
             <span className="text">I make the best cake you</span>
             <span className="mask"></span>
@@ -120,7 +120,7 @@ function LandingPage() {
             <span className="text">I make the best cake you</span>
             <span className="mask"></span>
           </h3>
-        </div>
+        </Parallax>
         {/*  */}
         <div className="text__wrap__two anim-text">
           <h3 className="text__head">
@@ -209,6 +209,9 @@ function LandingPage() {
         </div>
         {/*  */}
       </section>
+      <footer className="footer">
+
+      </footer>
     </div>
   );
 }
